33003ba631045148c006444eee0108159dd5e601
remeh <remeh@remeh.fr>
2016-02-27 15:04:35 +0100

    Remove dependency to robfig/cron, replaced by a ticker.

--- app/main.go
+++ app/main.go
@@ -25,7 +25,6 @@ import (
 	"./db"
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/jsgoecke/go-wit"
-	"github.com/robfig/cron"
 )
 
 var api *anaconda.TwitterApi
@@ -59,19 +58,21 @@ func main() {
 	// Init WIT api
 	witclient = wit.NewClient(WIT_ACCESS_TOKEN)
 
-	// Init cron
-	c := cron.New()
-	c.AddFunc(ACTIONS_INTERVAL, bot)
-	c.Start()
-
 	// Init random
 	rand.Seed(time.Now().UnixNano())
 
-	fmt.Println("Hello world")
+	// Starts the wake up ticker
+	var d time.Duration
+	if d, err = time.ParseDuration(ACTIONS_INTERVAL); err != nil {
+		panic(fmt.Sprintf("Can't parse as duration the ACTIONS_INTERVAL config value: %s", ACTIONS_INTERVAL))
+	}
 
-	bot()
+	ticker := time.NewTicker(d)
 
-	select {} // block forever
+	// wake up and go to sleep forever ever and never. tintintin.
+	for range ticker.C {
+		bot()
+	}
 }
 
 func bot() {
